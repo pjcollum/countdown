b79b1cc8763bbed6754a2553e630187723f62544
pjcollum <petercollum@hotmail.co.uk>
2022-01-17 21:39:38 +0000

    Initial commit

---
+++ readme.md
@@ -0,0 +1,24 @@
+# Countdown Timer Challenge
+
+---
+
+Create a banner that consists of the following:
+
+Text and imagery as seen on the reference images.
+Countdown clock
+Dropdown menu with T&Cs
+
+- The countdown clock's code must be written in vanilla javascript (not ES6).
+- Usage of frameworks/libraries is prohibited.
+- Your code mustn't be minimised.
+- Plus points if you provide a github repo with comments around your code.
+
+The build has to be fully responsive (mobile/tablet/desktop) and has to work on a local machine out of the box.
+
+Default time is set to 14 days in the future
+
+---
+
+![countdown image](/countdown_image.png)
+
+##### [View the project here](https://pjcollum.github.io/countdown/)
---
+++ script.js
@@ -0,0 +1,60 @@
+// Toggle Dropdown
+function dropDown() {
+    var element = document.getElementById('t&c');       //on click add the hidden class to the div
+    element.classList.toggle("hidden");
+}
+
+//Countdown Timer
+const endDate = new Date(Date.parse("February 17, 2022 00:00:00"));    //enter end time of countdown, date.parse to convert string to number
+
+function getTimeRemaining(endDate) {
+    const total = Date.parse(endDate) - Date.parse(new Date());     //date.parse to convert the date string to a number
+    const hours = Math.floor((total / (1000 * 60 * 60)) % 24);      //converts milliseconds to hours
+    const minutes = Math.floor((total / 1000 / 60) % 60);           //converts milliseconds to minutes
+    const seconds = Math.floor((total / 1000) % 60);                //converts milliseconds to seconds
+    return {
+      total,
+      hours,
+      minutes,
+      seconds
+    };
+}
+  
+function initializeClock(id, endDate) {
+    const clock = document.getElementById("clockdiv");
+    const hoursId = document.getElementById('hours');
+    const minutesId = document.getElementById('minutes');
+    const secondsId = document.getElementById('seconds');
+
+    function updateClock() {
+        const t = getTimeRemaining(endDate);
+
+        hoursId.innerHTML = ('0' + t.hours).slice(-2);  //if one digit, place a zero in front
+        minutesId.innerHTML = ('0' + t.minutes).slice(-2);
+        secondsId.innerHTML = ('0' + t.seconds).slice(-2);
+
+        if (t.total <= 0) {                     //keeps timer at zero when countdown has been reached
+           clearInterval(timeinterval);
+        }
+    }
+    
+    updateClock();
+    const timeinterval = setInterval(updateClock, 1000); //update timers updateClock() every second
+}
+
+initializeClock('clockdiv', endDate);
+
+
+//Solution 2 - prints countdown to console
+
+const d1 = new Date();
+const d2 = new Date(Date.parse("February 17, 2022 00:00:00"));
+console.log(d2-d1);
+const msDiff = d2 - d1;
+const daysDiff = msDiff/1000/60/60/24;
+console.log(daysDiff);
+
+const hours1 = Math.floor((msDiff / (1000 * 60 * 60)) % 24);      //converts milliseconds to hours
+const minutes1 = Math.floor((msDiff / 1000 / 60) % 60);           //converts milliseconds to minutes
+const seconds1 = Math.floor((msDiff / 1000) % 60);   
+console.log(hours1 + " hours " + minutes1 + " minutes " + seconds1 + " seconds");
